074f63019403c210e7d25fc6617059a188966276
Varun <bhat.varun.25@gmail.com>
2026-01-18 11:57:33 +0530

    top_p parameter added

--- single_prompt_chat_model_topp.py
+++ single_prompt_chat_model_topp.py
@@ -1,11 +1,12 @@
 from langchain_community.chat_models import ChatOllama
 
+
 # Create a chat Ollama Model instance
 model = ChatOllama(model = "llama3")
 
 # Invoke the model with a prompt
-response = model.invoke("Explain generative AI in one line", temperature = 0.7, top_k = 1)
+response = model.invoke("Explain generative AI in one line", temperature = 0.7, top_k = 50, top_p = 0.7)
 
-# Print the result
+# print the response
 print("Complete response: ", response)
-print("Response Content: ", response.content)
+print("Response content: ", response.content)
